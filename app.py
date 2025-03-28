68264eccd3a917bdb43c2ec6c342fbd0dccca1f6
lxlxlxhere <664942335@qq.com>
2025-03-28 12:51:14 +0000

    1

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import sqlite3
 import datetime
 import google.generativeai as genai
 import os
+import wikipedia
 
 app = Flask(__name__)
 
@@ -34,6 +35,14 @@ def main():
 def foodexp():
     return(render_template("foodexp.html"))
 
+@app.route("/foodexp1",methods=["POST","GET"])
+def foodexp1():
+    return(render_template("foodexp1.html"))
+
+@app.route("/foodexp2",methods=["POST","GET"])
+def foodexp2():
+    return(render_template("foodexp2.html"))
+
 @app.route("/foodexp_pred",methods=["POST","GET"])
 def foodexp_pred():
     q = float(request.form.get("q"))
@@ -60,6 +69,12 @@ def FAQ():
 def FAQ1():
     r = model.generate_content("Factores for Profit")
     return(render_template("FAQ1.html", r=r.candidates[0].content.parts[0]))
+
+@app.route("/FAQinput",methods=["POST","GET"])
+def FAQinput():
+    q = request.form.get("q")
+    r = wikipedia.summary(q)
+    return(render_template("FAQinput.html", r=r))
 
 @app.route("/userLog",methods=["POST","GET"])
 def userLog():
